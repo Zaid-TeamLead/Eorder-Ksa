a00cbd4e07c79319be010cefd5e3d6ed4d589312
sothebysbishal <bkh@sothebysrealty.ae>
2025-11-05 16:20:20 +0400

    Added Dashboard parts to the applications

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -3,6 +3,9 @@ import PageLoader from "@/components/loader/page-loader";
 import { useSession } from "@/lib/auth-client";
 import { redirect } from "next/navigation";
 import React, { useEffect } from "react";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const { data: session, isPending } = useSession();
@@ -21,7 +24,28 @@ const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return null;
   }
 
-  return <div>{children}</div>;
+  return (
+    <div>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          <div className="flex flex-1 flex-col min-h-screen">
+            <div className="@container/main flex flex-1 flex-col gap-2">
+              {children}
+            </div>
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
 };
 
 export default layout;
